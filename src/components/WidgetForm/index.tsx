ef0fc6a55e95c17e8b1bf792b7df802de34ff202
BrHCastro <hendecastro@gmail.com>
2022-05-09 20:59:23 -0300

    first commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+import bugImageUrl from '../../assets/icons/bug.svg';
+import ideaImageUrl from '../../assets/icons/idea.svg';
+import otherImageUrl from '../../assets/icons/other.svg';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lampada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: otherImageUrl,
+      alt: 'Imagem de uma nuvem de pensamento',
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] sm:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onSelect={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedBackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ por{' '}
+        <a
+          className="underline underline-offset-2"
+          href="https://brhcastro.com.br"
+          target="_blank"
+        >
+          BrHCastro
+        </a>
+      </footer>
+    </div>
+  );
+}
